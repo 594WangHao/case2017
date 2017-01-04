3e89e697f44b750689470c5b6de4b6f95c5715a3
594WangHao <wanghao594@hotmail.com>
2017-01-05 00:25:41 +0800

    新的结构，框架的使用是很必要的，条理结构清晰才能事半功倍，linux提上日程

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var cleanCSS = require('gulp-clean-css');
+var concat = require('gulp-concat');
+// var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+
+gulp.task('css', function() {
+    return gulp
+        .src('./less/case2017.less')
+        .pipe(less())
+        .pipe(concat('case2017.css'))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('./www/css/'))
+});
+
+gulp.task('js', function () {
+    gulp.src('./js/case2017.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./www/js/'))
+})
+
+gulp.task('watch', function () {
+    gulp.watch('./less/*.less', function () {
+        gulp.run('css')
+    })
+    gulp.watch('./js/*.js', function () {
+        gulp.run('js')
+    })
+})
+
+gulp.task('default', function () {
+    gulp.start('css','js','watch')
+})
